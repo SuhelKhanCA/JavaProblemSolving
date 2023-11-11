dac6939727f0c984cd14a439fc76dcfe3b40cd9e
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2023-11-12 00:35:50 +0530

    Update

---
+++ BoolDataTypes.java
@@ -0,0 +1,8 @@
+// code Explaining Boolean data types 
+public class Demo {
+   public static void main(String[] args) {
+       boolean t = true;
+       System.out.println(t);
+       boolean f = false;
+       System.out.println(f);
+   }}
