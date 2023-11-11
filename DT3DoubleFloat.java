9402ec31cfb181c23bb2923204aacf76dd97539b
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2023-11-12 00:33:05 +0530

    Decimal DataTypes in java

---
+++ DT3DoubleFloat.java
@@ -0,0 +1,12 @@
+// Code Explaining Decimal data types in java
+public class Demo {
+    public static void main(String[] args)
+    {
+        // float type
+        float f = 20.25f;
+        System.out.println("f= " + f);
+        // double type
+        double d = 20.25;
+        System.out.println("d= " + d);
+    }
+}
