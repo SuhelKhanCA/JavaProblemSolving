5ed4a2712a8424a7d7914556988b0c93098c2d98
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2023-11-12 00:34:05 +0530

    Update

---
+++ DtChar.java
@@ -0,0 +1,11 @@
+// Code Explaining Char data types 
+public class Demo {
+ public static void main(String[] args) {
+        char ch = 'S';
+       System.out.println(ch);
+        char ch2 = '&';
+       System.out.println(ch2);
+        char ch3 = '$';
+       System.out.println(ch3);
+   }
+}
