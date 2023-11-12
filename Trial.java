a2032631790e390ab76c0f292897b44571febd58
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2023-11-12 21:10:13 +0530

    Update

---
+++ Trial.java
@@ -0,0 +1,13 @@
+ /*** File name: Trial.java ***/
+class ForGeeks {
+    public static void main(String[] args)
+    {
+        System.out.println("For Geeks class");
+    }
+}
+class GeeksTest {
+    public static void main(String[] args)
+    {
+        System.out.println("Geeks Test class");
+    }
+}
