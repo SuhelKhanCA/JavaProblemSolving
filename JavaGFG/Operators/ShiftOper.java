3519b1a1c8574edcde99eca74da5b131b7db2c5c
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2023-11-12 20:54:25 +0530

    Update Operators

---
+++ JavaGFG/Operators/ShiftOper.java
@@ -0,0 +1,16 @@
+// Java Program to implement
+// shift operators
+import java.io.*;
+// Driver Class
+class GFG {
+    // main function
+    public static void main(String[] args)
+    {
+        int a = 10;
+           // using left shift
+        System.out.println("a<<1 : " + (a << 1));
+         // using right shift
+        System.out.println("a>>1 : " + (a >> 1));
+        System.out.println(a>>>1);
+    }
+}
