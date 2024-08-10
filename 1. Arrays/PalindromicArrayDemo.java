4fed5b67c7525f37706f8a9785c4acf05fab9f60
suhelkhanca <suhel.hata@gmail.com>
2024-08-10 10:36:13 +0530

    Palindromic array

--- 1. Arrays/PalindromicArrayDemo.java
+++ 1. Arrays/PalindromicArrayDemo.java
@@ -1,45 +1,40 @@
-package Arrays;
-
 //{ Driver Code Starts
-    import java.util.*;
-    class PalindromicArray{
-        public static void main(String[] args){
-            Scanner sc = new Scanner(System.in);
-            int t = sc.nextInt();
-            while(t-->0)
-            {
-                int n = sc.nextInt();
-                int[] a = new int[n];
-                for(int i = 0 ;i < n; i++)
-                    a[i]=sc.nextInt();
-                GfG g = new GfG();
-                System.out.println(g.palinArray(a , n));
-            }
+import java.util.*;
+
+class PalindromicArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = sc.nextInt();
+            PalindromicArrayDemo g = new PalindromicArrayDemo();
+            System.out.println(g.palinArray(a, n));
         }
     }
-    // } Driver Code Ends
-    
-    
-    /*Complete the Function below*/
-    class PalindromicArrayDemo
-    {
-        public static int palinArray(int[] a, int n)
-               {
-                      //add code here.
-                      int ans = 1;
-                for(int i=0; i< n-1; i++){
-                    int rev = 0;
-                    int num = a[i];
-                    while(a[i] != 0){
-                        int r = a[i] % 10;
-                        a[i] = a[i]/10;
-                        rev = 10* rev + r;
-                    }
-                    if(num != rev){
-                        ans = 0;
-                        break;
-                    }
-                }
-                return ans;
-               }
+}
+// } Driver Code Ends
+
+/* Complete the Function below */
+class PalindromicArrayDemo {
+    public static boolean palinArray(int[] a, int n) {
+        // add code here.
+        boolean ans = true;
+        for (int i = 0; i < n - 1; i++) {
+            int rev = 0;
+            int num = a[i];
+            while (a[i] != 0) {
+                int r = a[i] % 10;
+                a[i] = a[i] / 10;
+                rev = 10 * rev + r;
+            }
+            if (num != rev) {
+                ans = false;
+                break;
+            }
+        }
+        return ans;
     }
+}
