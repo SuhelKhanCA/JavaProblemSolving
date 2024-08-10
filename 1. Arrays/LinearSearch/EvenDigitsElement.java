56500c4fa06a31425db8f353baaa9192dd75850e
suhelkhanca <suhel.hata@gmail.com>
2024-08-10 15:34:09 +0530

    Numbers having even no. of digits -ve condition included

--- 1. Arrays/LinearSearch/EvenDigitsElement.java
+++ 1. Arrays/LinearSearch/EvenDigitsElement.java
@@ -5,6 +5,9 @@ class Solution {
     public int findNumbers(int[] nums) {
         int count  = 0;
         for(int i = 0; i < nums.length; i++){
+            if (nums[i] < 0) {
+                nums[i] *= -1;
+            }
             if(isEven(nums[i])){
                 count++;
             }
