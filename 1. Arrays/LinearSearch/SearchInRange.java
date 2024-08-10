ef11fb974272ce06ec464b6341fb95e07f8456c0
suhelkhanca <suhel.hata@gmail.com>
2024-08-10 14:34:04 +0530

    Search in the specied index range

--- 1. Arrays/LinearSearch/SearchInRange.java
+++ 1. Arrays/LinearSearch/SearchInRange.java
@@ -5,16 +5,16 @@ public class SearchInRange {
         int arr[] = {18, 12, -7, 3, 14, 28}; // search for 3 in the range of index 1 till 4
         int target = 3;
 
-        System.out.println("Is target there in the specified range: " + search(arr, target));
+        System.out.println("Is target there in the specified range: " + search(arr, target, 1, 4));
     }
 
-    static boolean search(int[] a, int target){
+    static boolean search(int[] a, int target, int start, int end){
 
         if (a.length == 0) {
             return false;
         }
 
-        for (int i = 0; i < a.length; i++) {
+        for (int i = start; i < end; i++) {
             if (a[i] == target) {
                 return true;
             }
