6329660b58d027555fd34304e3ffbfb56111186a
suhelkhanca <suhel.hata@gmail.com>
2024-08-18 17:10:18 +0530

    bubble sort

--- 1. Arrays/SimpleSorting/BubbleSort.java
+++ 1. Arrays/SimpleSorting/BubbleSort.java
@@ -6,7 +6,7 @@ class BubbleSort{ // Syncing sort OR Exchange sort
         int n = arr.length -1;
         for (int i = 0; i < n; i++)
         {
-            for (int j = 0; j < n; j++)
+            for (int j = 0; j < n-i; j++)
             {
                 if (arr[j] > arr[j+1])
                 {
