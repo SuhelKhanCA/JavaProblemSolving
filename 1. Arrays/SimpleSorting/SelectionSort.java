6b1ec2c34dd28485d1982fc2a7213ed9cac22293
suhelkhanca <suhel.hata@gmail.com>
2024-08-18 17:52:00 +0530

    Selection sort

--- 1. Arrays/SimpleSorting/SelectionSort.java
+++ 1. Arrays/SimpleSorting/SelectionSort.java
@@ -1,20 +1,22 @@
 import java.util.*;
-class SelectionSort{
+class SelectionSort{ // It's not stable sorting algo  // Performs well on  small lists
     public static void main(String[] args) {
         int[] arr = {64, 34, 25, 12, 22, 11, 90};
         int n = arr.length;
-
-            for (int i = 0; i < n; i++) {
+        selectionSort(arr, n);
+        System.out.println("Sorted Array : " + Arrays.toString(arr));
+    }
+    static void selectionSort(int[] arr, int n){
+        for (int i = 0; i < n; i++) {
                 int min_idx = i;
                 for (int j = i + 1; j < n; j++) {
                     if (arr[min_idx] > arr[j]) {
                         min_idx = j;
                     }
                 }
-                        int temp = arr[i];
-                        arr[i] = arr[min_idx];
-                        arr[min_idx] = temp;
+                int temp = arr[i];
+                arr[i] = arr[min_idx];
+                arr[min_idx] = temp;
             }
-        System.out.println("Sorted Array : " + Arrays.toString(arr));
     }
 }
