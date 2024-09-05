45cc7e4acd948dcffe37b6935b83293ca567a572
suhelkhanca <suhel.hata@gmail.com>
2024-09-05 08:11:01 +0530

    Illustration 1

---
+++ 4. Recursion/Example.java
@@ -0,0 +1,24 @@
+/**
+ * Example
+ */
+ class Example {
+
+    public static void main(String[] args) {
+        // call the function
+        message();
+    }
+
+    static void message(){
+        System.out.println("Hello, World!");
+        message1();
+    }
+    static void message1(){
+        System.out.println("Hello, World!");
+        message3();
+    }
+    static void message3(){
+        System.out.println("Hello, World!");
+    }
+    
+    
+}
