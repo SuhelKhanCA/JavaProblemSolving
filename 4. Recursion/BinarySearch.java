10f62d17c4cac2be9d2af4d659288ba0463a7e32
suhelkhanca <suhel.hata@gmail.com>
2024-09-05 12:22:07 +0530

    Binary search using recursion

--- 4. Recursion/BinarySearch.java
+++ 4. Recursion/BinarySearch.java
@@ -1,5 +1,23 @@
 class BinarySearchRecursion {
     public static void main(String[] args) {
-        
+        int[] arr = {1, 2,3,4,5,6,7,8,9};
+        System.out.println(search(arr, 3, 0, arr.length-1));
+    }
+
+    static int search(int[] arr, int target, int s, int e){
+        if(s > e){
+            return -1;
+        }
+
+        int m = s + (e-s)/2;
+
+        if (target == arr[m]) {
+            return m;
+        }
+        else if (target > arr[m]) {
+            return search(arr, target, m+1, e);
+        }else{
+            return search(arr, target, s, m-1);
+        }
     }
 }
