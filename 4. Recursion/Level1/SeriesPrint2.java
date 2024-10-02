f5bcba699050b80302bf7fb65cfcb7416fe53c9b
suhelkhanca <suhel.hata@gmail.com>
2024-10-02 22:04:49 +0530

    printing from 1 to n

---
+++ 4. Recursion/Level1/SeriesPrint2.java
@@ -0,0 +1,20 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class SeriesPrint2 {
+    public static void print(int n){
+        if (n==1) {
+            System.out.print(1);
+            return;
+        }
+        print(n - 1);
+        System.out.print(n + " ");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value value of n: ");
+        int n = sc.nextInt();
+        print(n);
+    }
+}
