7ead57dd6a2520ff78774fa5492bc15dd0785bfa
suhelkhanca <suhel.hata@gmail.com>
2024-10-03 00:01:54 +0530

    reverse a number

---
+++ 4. Recursion/Level1/CountZero.java
@@ -0,0 +1,18 @@
+package Level1;
+
+public class CountZero {
+    static int count(int n, int c){
+        if(n%10 == n){
+            return c;
+        }
+        if (n%10 == 0) {
+            return count(n/10, c++);
+        }else{
+            return count(n/10, c);
+        }
+    }
+    public static void main(String[] args) {
+        int n = 10204;
+        System.out.println(count(n, 0));
+    }
+}
--- 4. Recursion/Level1/Reverse.java
+++ 4. Recursion/Level1/Reverse.java
@@ -1,14 +1,31 @@
 package Level1;
 
 public class Reverse {
-    static int rev(int n){
+    static int reverse = 0;
+    static void rev(int n){
+        if (n == 0) {
+            return;
+        }
+        int rem = n % 10;
+        reverse = 10 * reverse + rem;
+        rev(n/10);
+    }
+    // f(N, args) = rem*10^arg-1 + f(N/10, arg-1)
+    static int rev2(int n){
+        int digits = (int)(Math.log10(n)) + 1;
+        return helper(n, digits);
+    }
+    static int helper(int n, int digits){
         if (n == 0) {
             return 0;
         }
-        return 10 * (n%10) + rev(n/10);
+        int rem = n%10;
+        return rem * (int)Math.pow(10, digits) + helper(n/10, digits-1);
     }
     public static void main(String[] args) {
         int n= 1234;
-        System.out.println(rev(n));
+        // rev(n);
+        // System.out.println(reverse);
+        System.out.println(rev2(n));
     }
 }
