44d132a3d15b1ed49910a3cbe7c73877fd400b60
suhelkhanca <suhel.hata@gmail.com>
2024-11-03 18:36:09 +0530

    dynamic stack using array

--- 7. Stacks Queues/DynamicStack.java
+++ 7. Stacks Queues/DynamicStack.java
@@ -2,5 +2,63 @@ public class DynamicStack extends CustomStack {
     public DynamicStack() {
         super();
     }
-    // public
+
+    public DynamicStack(int size) {
+        super(size);
+    }
+
+    @Override
+    public boolean push(int item) {
+        if (this.isFull()) {
+            // double the array size
+            int[] temp = new int[data.length * 2];
+            // copy all the previous elements
+            for (int i = 0; i < data.length; i++) {
+                temp[i] = data[i];
+            }
+            data = temp;
+        }
+        // ptr++;
+        // data[ptr] = item;
+        super.push(item);
+        return true; 
+    }
+    // isFull
+    public boolean isFull() {
+        return ptr == data.length - 1;
+    }
+
+    public static void main(String[] args) {
+        DynamicStack stack = new DynamicStack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(44);
+        stack.push(65);
+        stack.push(85);
+        stack.push(25);
+        stack.push(57);
+
+        stack.printStack();
+        System.out.println();
+
+        stack.pop();
+        System.out.println();
+        stack.printStack();
+
+        stack.push(25);
+        stack.push(57);
+        
+        stack.printStack();
+        System.out.println();
+        
+        stack.push(547);
+        stack.push(855);
+        stack.push(14);
+        stack.push(967);
+        stack.push(5);
+        stack.push(7);
+        stack.push(507);
+        System.out.println();
+        stack.printStack();
+    }
 }
