50786168bc13d3eaef055e2b90dd299ec14dffef
suhelkhanca <suhel.hata@gmail.com>
2024-11-07 01:58:18 +0530

    Insertion and traversal of doubly linked list

--- 3. LinkedList/DLL/DLL.java
+++ 3. LinkedList/DLL/DLL.java
@@ -32,6 +32,7 @@ public class DLL {
         if (head == null && tail == null) {
             head = node;
             tail = node;
+            size++;
             return;
         }
 
@@ -40,10 +41,80 @@ public class DLL {
         node.prev = null;
 
         head = node;
+        size++;
+    }
+
+    // insert at last
+    public void insertLast(int value) {
+        Node node = new Node(value);
+
+        if (tail == null || head == null) {
+            head = tail = node;
+            size++;
+            return;
+        }
+
+        tail.next = node;
+        node.prev = tail;
+        node.next = null;
+
+        tail = node;
+        size++;
+    }
+    
+    // insert at any position
+
+    public void insertAtIndex(int value, int index) {
+        if (index == 0) {
+            insertFirst(value);
+            return;
+        }
+        if (index == size - 1) {
+            insertLast(value);
+            return;
+        }
+        Node node = new Node(value);
+        Node tmp = head;
+        for (int i = 1; i < index; i++) {
+            tmp = tmp.next;
+        }
+
+        node.next = tmp.next;
+        node.prev = tmp;
+        tmp.next = node;
+        tmp.next.prev = node;
+    }
+    // find
+    public Node find(int value) {
+        Node node = head;
+
+        while (node != null) {
+            if (node.value == value) {
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+    // insert after a value
+    public void insert(int after, int value) {
+        Node p = find(after);
+        if (p == null) {
+            System.out.println("Does not exist");
+            return;
+        }
+        Node node = new Node(value);
+        
+        node.next = p.next;
+        node.prev = p;
+        if (node.next != null) {
+            p.next.prev = node;
+        }
+        p.next = node;
     }
 
     // Display method
-    public void diaplay() {
+    public void display() {
         Node node = head;
 
         while (node != null) {
@@ -54,7 +125,7 @@ public class DLL {
     }
 
     // reverse display
-    public void diaplayReverse() {
+    public void displayReverse() {
         Node node = tail;
 
         while (node != null) {
@@ -74,7 +145,12 @@ public class DLL {
         list.insertFirst(22);
         list.insertFirst(33);
 
-        list.diaplay();
-        list.diaplayReverse();
+        list.display();
+        // list.displayReverse();
+        list.insertAtIndex(100, 2);
+        list.display();
+
+        list.insert(100, 99);
+        list.display();
     }
 }
