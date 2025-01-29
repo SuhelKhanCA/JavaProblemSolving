8dc8838820c874ed20354a6c12ee2b752644958e
suhelkhanca <suhel.hata@gmail.com>
2025-01-29 22:13:14 +0530

    Create LongestRepeatingCharacterReplacement.java

---
+++ 2. Strings/Problems-15/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/longest-repeating-character-replacement/
+
+public class LongestRepeatingCharacterReplacement {
+    public int characterReplacement(String s, int k) {
+        int[] occurance = new int[26];
+        int left = 0, right = 0;
+        int ans = 0;
+        int maxOccurence = 0;
+
+        while (right < s.length()) {
+            maxOccurence = Math.max
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
